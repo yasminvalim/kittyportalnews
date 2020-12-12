aeffdf205c51a53b093193e355e69ec8a13047cc
YasminValim <yasmin.valimds@gmail.com>
2020-12-12 04:27:38 -0300

    New feature create new adm

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,42 @@
+class UserController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user, except: [:index]
+    
+    def index
+      @users = User.all
+    end
+  
+    def show
+    end
+    
+    def edit
+    end
+  
+    def update
+      respond_to do |format|
+        if @user.update(user_params)
+          format.html { redirect_to users_path, notice: 'user was successfully updated.' }
+        else
+          format.html { render :edit }
+        end
+      end
+    end
+  
+    def destroy
+      @user.destroy
+      respond_to do |format|
+        format.html { redirect_to users_path, notice: 'user was successfully destroyed.' }
+      end
+    end
+  
+    private
+      def set_user
+        @user = User.find(params[:id])
+      end
+  
+      def user_params
+        params.require(:user).permit(:email, :is_admin)
+      end
+  end
+
+end

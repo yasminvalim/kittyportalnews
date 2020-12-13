853176fea3e07586acf3da5f3e6fa8a72a78bddd
YasminValim <yasmin.valimds@gmail.com>
2020-12-13 17:13:32 -0300

    Search for users!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
 before_action :set_search
+before_action :set_search_user
 
 def set_search
   @q = Post.ransack(params[:q])
+end
+
+def set_search_user
+  @quser = User.ransack(params[:q])
 end
 
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,6 @@
 class WelcomeController < ApplicationController
 
   def index
-    @q = Post.ransack(params[:q])
     @posts = @q.result.order(created_at: :desc).page params[:page]
   end
 
